a3e7673ea24f87a8b3bb7e0a17fb584070b0e54c
Kauê Luí <ofckaue@gmail.com>
2023-11-12 01:05:07 -0300

    Criando personagem

---
+++ js/character.js
@@ -0,0 +1,23 @@
+class Character{
+    constructor({position, dimensions}){
+        this.position = position
+        this.width = dimensions.width
+        this.height = dimensions.height
+    }
+
+    draw(){
+        ctx.fillStyle = "white"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Character({
+    position: {
+        x: 100,
+        y: 100
+    },
+    dimensions:{
+        width: 50,
+        height: 150
+    }
+})
